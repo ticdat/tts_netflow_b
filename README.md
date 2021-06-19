9fc4f761ea5e2c8ed5f343fdf2934bcf3bd054e1
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2021-06-19 15:23:50 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tts_netflow_b
+Multi-commodity network flow Tidy, Tested, Safe demonstration repository.
---
+++ setup.py
@@ -0,0 +1,11 @@
+import tts_netflow_b
+from setuptools import setup, find_packages
+
+setup(
+   name = 'tts_netflow_b',
+   packages = find_packages(),
+   version = tts_netflow_b.__version__,
+   author = 'Pete Cacioppi',
+   url = 'https://github.com/ticdat/tts_netflow_b',
+   platforms = 'any',
+)
